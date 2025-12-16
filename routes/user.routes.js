09650ac1b5baebdd2a13c0eab7865002899bcdac
Srinjay Saha <srinjay.saha.career@gmail.com>
2025-12-16 22:08:47 +0530

    Day 2

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test',(req,res)=>{
-    res.send('User Test Route');
+router.get('/register',(req,res)=>{
+    res.render('register');
 });
 
 module.exports=router;
